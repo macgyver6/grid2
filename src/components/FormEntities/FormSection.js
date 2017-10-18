60717e3d94fb113afea8c345e729c3d13c965356
Adam <adamfranzen@gmail.com>
2017-10-18 13:55:57 -0400

    raised state and event listeners to FormSection to accomodate any FormEntity resize

--- src/components/FormEntities/FormSection.js
+++ src/components/FormEntities/FormSection.js
@@ -10,6 +10,58 @@ class FormSectionComponent extends Component {
     this.dragover_handler = this.dragover_handler.bind(this);
     this.dragend_handler = this.dragend_handler.bind(this);
     this.dragstart_handler = this.dragstart_handler.bind(this);
+    this.mouseDownHandler = this.mouseDownHandler.bind(this);
+    this.mouseUpHandler = this.mouseUpHandler.bind(this);
+
+    this.state = {
+      resize: {
+        init: null,
+        changed: null
+      }
+    }
+  }
+
+  mouseDownHandler(event) {
+    if (event.target.className === 'resizer') {
+      event.preventDefault();
+      event.stopPropagation();
+      const resize = this.state.resize;
+      resize.init = event.screenX;
+      this.setState({ resize })
+      // window.addEventListener('mouseup', this.mouseUpHandler);
+    }
+  }
+
+  mouseUpHandler(event) {
+    const resize = this.state.resize;
+    resize.changed = event.screenX;
+    let initX = this.state.resize.init
+    let initGrid = this.props.model.children()[0].width()
+    let resizeX = event.screenX
+    let address = utility.findNode(this.props.model.children()[0], this.props.form)
+    let initDiff = resizeX - initX
+    let fsWidth = parseInt((document.getElementById(this.props.model.UUID()).clientWidth / 24), 10)
+    let diffGrid = (parseInt(((Math.abs(initDiff)) / fsWidth), 10))
+    if ((Math.abs(initDiff)) > 20) {
+      this.setState({ resize })
+      var calcOpp = {
+        '+': (a, b) => a + b,
+        '-': (a, b) => a - b
+      }
+      if (initDiff > 0) {
+        calc.call(this, calcOpp['+'](initGrid, diffGrid))
+      } else {
+        calc.call(this, calcOpp['-'](initGrid, diffGrid))
+      }
+    }
+
+    function calc(newWidth) {
+      let entityToChange = this.props.model.children()[0]
+      this.props.removeformentity(address)
+      this.props.addformentity(
+        entityToChange.mutate({ width: (newWidth) }), address
+      )
+    }
   }
 
   handleDelete = function (event, props) {
@@ -23,14 +75,12 @@ class FormSectionComponent extends Component {
 
   dragstart_handler = function (event) {
     event.stopPropagation();
-    console.log(this.props.model._uuid)
     event.dataTransfer.setData("text/plain", JSON.stringify(this.props.model.properties()));
   }
 
   drop_handler(event) {
     event.preventDefault();
     event.stopPropagation();
-    console.log('drophandler 2')
     let data = event.dataTransfer.getData("text");
     let entityToAdd = utility.resurrectEntity(defaultPropsFE[data])
     let location = utility.findNode(this.props.model, this.props.form)
@@ -66,6 +116,9 @@ class FormSectionComponent extends Component {
         draggable="true"
         onDragEnd={this.dragend_handler}
         onDragStart={this.dragstart_handler}
+        onMouseDown={(e) => this.mouseDownHandler(e, this.props)}
+        onMouseUp={(e) => this.mouseUpHandler(e, this.props)}
+        id={this.props.model.UUID()}
         >
         {this.props.model.children().map((element, i) => {
           return React.createElement(utility.lookupComponent(element), { key: i, model: element, form: this.props.form, removeformentity: this.props.removeformentity, addformentity: this.props.addformentity })
--- src/components/FormEntities/TextArea.js
+++ src/components/FormEntities/TextArea.js
@@ -11,18 +11,10 @@ let handleDelete = function (event, props) {
 class TextAreaComponent extends React.Component {
   constructor(props) {
     super(props);
-
-    this.state = {
-      resize: {
-        init: null,
-        changed: null
-      }
-    }
     this.handleChange = this.handleChange.bind(this);
     this.dragend_handler = this.dragend_handler.bind(this);
     this.dragstart_handle = this.dragstart_handle.bind(this);
-    this.mouseDownHandler = this.mouseDownHandler.bind(this);
-    this.mouseUpHandler = this.mouseUpHandler.bind(this);
+
   }
 
   handleChange(event, props) {
@@ -39,58 +31,6 @@ class TextAreaComponent extends React.Component {
   dragstart_handle(event) {
     this.event.stopPropagation();
     this.event.dataTransfer.setData("text/plain", JSON.stringify(this.props.model.properties()));
-  }
-
-  mouseDownHandler(event) {
-    if (event.target.className === 'resizer') {
-      event.preventDefault();
-      event.stopPropagation();
-      const resize = this.state.resize;
-      resize.init = event.screenX;
-      this.setState({ resize })
-      window.addEventListener('mouseup', this.mouseUpHandler);
-    }
-  }
-
-  mouseUpHandler(event) {
-    const resize = this.state.resize;
-    resize.changed = event.screenX;
-    let initX = this.state.resize.init
-    let initGrid = this.props.model.width()
-    let resizeX = event.screenX
-    let address = utility.findNode(this.props.model, this.props.form)
-    let initDiff = resizeX - initX
-    console.log(initDiff)
-    let diffGrid = parseInt(((Math.abs(initDiff)) / 24), 10)
-    console.log(diffGrid)
-    if ((Math.abs(initDiff)) > 20) {
-      this.setState({ resize })
-      var calcOpp = {
-        '+': (a, b) => a + b,
-        '-': (a, b) => a - b
-      }
-      if (initDiff > 0) {
-        calc.call(this, calcOpp['+'](initGrid, diffGrid))
-      } else {
-        calc.call(this, calcOpp['-'](initGrid, diffGrid))
-      }
-    }
-
-    function calc(newWidth) {
-      this.props.removeformentity(address)
-      this.props.addformentity(
-        this.props.model.mutate({ width: (newWidth) }), address
-      )
-    }
-  }
-
-  componentDidMount() {
-    window.addEventListener('mousedown', this.mouseDownHandler);
-  }
-
-  componentWillUnmount() {
-    window.removeEventListener('mousedown', this.mouseDownHandler);
-    window.removeEventListener('mouseup', this.mouseUpHandler);
   }
 
   render() {
@@ -116,7 +56,6 @@ class TextAreaComponent extends React.Component {
       <div
         className="resizer"
         style={resizeStyle}
-        ref={elem => this.resizer = elem}
       >
       </div>
 
@@ -126,8 +65,8 @@ class TextAreaComponent extends React.Component {
         draggable="true"
         onDragEnd={this.dragend_handler}
         onDragStart={this.dragstart_handler}
-        onClick={(e) => this.handleChange(e, this.props)}
       >
+        {/* onClick={(e) => this.handleChange(e, this.props)} */}
         <p>{this.props.model.width()}</p>
         <textarea className="form-control" placeholder="Write something in text area" name={this.props.model.name()} rows={this.props.model.numRows()} cols={this.props.model.numColumns()} type={this.props.model.type()}>
         </textarea>
