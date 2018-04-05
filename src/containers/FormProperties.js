56a2aecede9439c2eca8c5fa805ca389d4bbb9c2
Adam Franzen <adamfranzen@gmail.com>
2018-04-05 17:21:42 -0400

    image file working...finally

--- src/containers/FormProperties.js
+++ src/containers/FormProperties.js
@@ -6,71 +6,21 @@ export const FormProperty = props => {
     event.preventDefault();
   };
 
-  const saveFileToLocal = (file, name) => {
-    var fileReader = new FileReader();
-    const existing = localStorage.getItem('FILE');
-    fileReader.readAsDataURL(file);
-    fileReader.onload = evt => {
-      var result = evt.target.result;
-      if (existing === null) {
-        try {
-          console.log([{ [name]: result }]);
-          localStorage.setItem('FILE', JSON.stringify([{ [name]: result }]));
-        } catch (e) {
-          console.log('Storage failed: ' + e);
-        }
-      } else {
-        // console.log(JSON.parse(existing).concat([{ [name]: result }]))
-        // console.log(existing)
-        localStorage.setItem(
-          'FILE',
-          JSON.stringify(JSON.parse(existing).concat([{ [name]: result }]))
-        );
-      }
-    };
-  };
-
-  const handleFiles = files => {
-    const item = files.getAsFile();
-
-    // Create XHR, Blob and FileReader objects
-    var xhr = new XMLHttpRequest();
-
-    xhr.open('GET', '/pdf/download?id=');
-    xhr.responseType = 'blob';
-    xhr.onload = () => {
-      console.log(files); // response as a blob
-      if (xhr.status && xhr.status === 200) {
-        saveFileToLocal(xhr.response, item.name);
-      }
-    };
-
-    xhr.send();
+  const saveToLocal = readFile => {
+    localStorage.setItem('Files', readFile);
   };
 
   const dropFile_handler = event => {
-    console.log('Drop');
     event.preventDefault();
-    // If dropped items aren't files, reject them
-    var dt = event.dataTransfer;
-    if (dt.items) {
-      // Use DataTransferItemList interface to access the file(s)
-      const f = [];
-      for (var i = 0; i < dt.items.length; i++) {
-        if (dt.items[i].kind === 'file') {
-          f.push(handleFiles(dt.items[i]));
-        }
-      }
-    }
+    var files = event.dataTransfer.files[0];
+    var reader = new FileReader();
+    reader.readAsDataURL(files);
+    reader.onload = function(readEvent) {
+      saveToLocal(readEvent.target.result);
+    };
   };
-  const stringOfFiles = localStorage.getItem('FILE');
-  const fileNames = stringOfFiles
-    ? JSON.parse(stringOfFiles).map(file => {
-        return Object.keys(file)[0];
-      })
-    : null;
+
   const change_handler = event => {
-    // console.log(event.target.value);
     const value =
       event.target.type === 'checkbox'
         ? event.target.checked
@@ -84,6 +34,8 @@ export const FormProperty = props => {
     height: '900px',
     border: '2px solid',
   };
+
+  const imageyolo = localStorage.getItem('FILE');
   return (
     <div
       style={formPropertiesStyle}
@@ -92,7 +44,8 @@ export const FormProperty = props => {
     >
       <h1>Form Properties</h1>
       <h2>File Uploaded</h2>
-      <ul>{fileNames.map(file => <li>{file}</li>)}</ul>
+      <img src={localStorage.getItem('Files')} />
+      {/* <ul>{fileNames.map(file => <li>{file}</li>)}</ul> */}
     </div>
   );
 };
--- src/reducers/form.reducer.js
+++ src/reducers/form.reducer.js
@@ -32,9 +32,9 @@ const formReducer = (state, action) => {
       form: new Form(defaultPropsFE.Form),
       app: {
         activeTab: 0,
-        currententity: [0, 0, 0]
+        currententity: [0, 0, 0],
         // currententity: null
-      }
+      },
     };
     return state;
   }
@@ -45,28 +45,27 @@ const formReducer = (state, action) => {
 
   if (action.type === 'INCREMENT') {
     return Object.assign({}, state, {
-      value: state.value + 1
+      value: state.value + 1,
     });
   }
 
   if (action.type === 'DECREMENT') {
     return Object.assign({}, state, {
-      value: state.value - 1
+      value: state.value - 1,
     });
   }
 
   if (action.type === 'ADD') {
     let result = utility.add(action.path, action.entity, state.form);
     return Object.assign({}, state, {
-      form: result
+      form: result,
     });
   }
 
   if (action.type === 'REMOVE') {
-    console.log(state.form, action.path);
-    let update = utility.remove(action.path, state.form);
+    let result = utility.remove(action.path, state.form);
     return Object.assign({}, state, {
-      form: update
+      form: result,
     });
   }
 
@@ -74,7 +73,7 @@ const formReducer = (state, action) => {
     const initEntity = address.byPath(state.form, action.path);
     let update = utility.remove(action.path, state.form);
     const removedUpdate = Object.assign({}, state, {
-      form: update
+      form: update,
     });
     let mutatedEntity = Object.assign(
       {},
@@ -87,7 +86,7 @@ const formReducer = (state, action) => {
       removedUpdate.form
     );
     return Object.assign({}, state, {
-      form: result
+      form: result,
     });
   }
 
@@ -95,7 +94,7 @@ const formReducer = (state, action) => {
     let mutatedEntity = state.form.setChildren(action.properties);
 
     return Object.assign({}, state, {
-      form: mutatedEntity
+      form: mutatedEntity,
     });
   }
 
@@ -103,7 +102,7 @@ const formReducer = (state, action) => {
     let serialized = comm.serialize(state.form);
     localStorage.setItem('model', JSON.stringify(serialized));
     return Object.assign({}, state, {
-      lastSaved: Date.now()
+      lastSaved: Date.now(),
     });
   }
 
